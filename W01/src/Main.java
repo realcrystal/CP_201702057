70f9e9761682c90acd9bac42200b69afd9b7cb2f
realcrystal <sjfs0709@gmail.com>
2017-11-19 20:45:08 +0900

    first assignment

---
+++ W01/src/Main.java
@@ -0,0 +1,38 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int lineNumber;
+		
+		lineNumber=1;
+		System.out.println(lineNumber+". 죽는 날까지 하늘을 우러러");
+		lineNumber=lineNumber+1;
+		System.out.println(lineNumber+". 한 점 부끄럼이 없기를,");
+		lineNumber=lineNumber+1;
+		System.out.println(lineNumber+". 잎새에 이는 바람에도");
+		lineNumber=lineNumber+1;
+		System.out.println(lineNumber+". 나는 괴로워했다.");
+		lineNumber=lineNumber+1;
+		System.out.println(lineNumber+". 별을 노래하는 마음으로");
+		lineNumber=lineNumber+1;
+		System.out.println(lineNumber+". 모든 죽어가는 것을 사랑해야지.");
+		lineNumber=lineNumber+1;
+		System.out.println(lineNumber+". 그리고 나한테 주어진 길을");
+		lineNumber=lineNumber+1;
+		System.out.println(lineNumber+". 걸어가야겠다.");
+		lineNumber=lineNumber+1;
+		System.out.println(lineNumber+". ");
+		lineNumber=lineNumber+1;
+		System.out.println(lineNumber+". 오늘 밤에도 별이 바람에 스치운다.");
+		lineNumber=lineNumber+1;
+		System.out.println(lineNumber+". ");
+		lineNumber=lineNumber+1;
+		System.out.println(lineNumber+". 서시, 윤동주");
+		
+//		좋아하는 시를 줄 번호와 함께 출력하기
+
+		
+	}
+
+}
